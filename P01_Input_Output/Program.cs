8d03e604cb3d7b0ed8631029e1f14e2db572db9d
yaolei <815141681@qq.com>
2019-10-13 18:45:05 +0800

    v

---
+++ P01_Input_Output/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//备注，快捷键 command+/
+namespace P01_Input_Output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "YaoLei";
+            Console.WriteLine("姓名为:{0}", name);  //输出语句学习
+            var age = Convert.ToInt32(Console.ReadLine());  //输出语句学习 
+            Console.WriteLine("年龄为：{0}", age);
+        }
+    }
+}
